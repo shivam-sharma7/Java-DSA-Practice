9a7263467b7ab44d006961999d8d2e6d0d484241
Shivam Sharma <meshivam81@gmail.com>
2022-12-31 17:33:12 +0530

    Binary search Q add

---
+++ Java-DSA-Practice/Binary_Search_04/src/BianrySearch_Algorithm_Decending.java
@@ -0,0 +1,38 @@
+public class BianrySearch_Algorithm_Decending {
+
+    public static void main(String[] args) {
+
+        int [] arr = {12, 9, 6, 5, 3, 2, -1};
+
+        int target = 9;
+
+        System.out.println(binary_search(arr, target));
+    }
+
+     static int binary_search(int [] arr, int target) {
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<=end){
+
+            int mid = start+(end-start)/2;
+
+            if(target== arr[mid]){
+
+                return mid;
+            }
+            else if (target > arr[mid]) {
+
+                end = mid - 1;
+            }
+
+            else {
+
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+     }
+}
---
+++ Java-DSA-Practice/Binary_Search_04/src/BinarySearch_Algorithms.java
@@ -0,0 +1,39 @@
+public class BinarySearch_Algorithms {
+    public static void main(String[] args)
+    {
+        System.out.println(" Hello Binary Search World!");
+
+        int [] arr = {2, 4, 9, 10, 12, 14, 18};
+
+        int target = 14;
+
+        System.out.println(binary_Search(arr, target));
+    }
+
+    static int binary_Search(int [] arr, int target){
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end){
+
+            int mid = start+(end- start)/2;
+
+            if(target == arr[mid]){
+
+                return mid;
+            }
+
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+
+            else {
+
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
