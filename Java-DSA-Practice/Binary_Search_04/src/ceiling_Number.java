7ffc2114d4d18055c3abad0c7a04e1b747a1610b
Shivam Sharma <meshivam81@gmail.com>
2023-01-01 09:32:19 +0530

    ceiling and floor of number Q add

---
+++ Java-DSA-Practice/Binary_Search_04/src/ceiling_Number.java
@@ -0,0 +1,40 @@
+public class ceiling_Number {
+
+    public static void main(String[] args) {
+
+        int [] arr = {2, 3, 44, 53, 56, 78, 98};
+
+        int target = 55;
+
+        System.out.println(ceiling_Number(arr, target));
+
+    }
+
+    static int ceiling_Number(int [] arr, int target){
+
+        int start = 0;
+        int end = arr.length;
+
+        while(start<=end){
+
+            int mid = start+(end - start)/2;
+
+            if(target == arr[mid]){
+
+                return mid;
+            }
+
+            else if (target > arr[mid]) {
+
+                start = mid + 1;
+            }
+
+            else{
+
+                end = end - 1;
+            }
+        }
+
+        return start;
+    }
+}
--- Java-DSA-Practice/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to your in Java DSA Bootcamp!" +"Stay Positive during solving various DSA Problem");
-    }
-}
